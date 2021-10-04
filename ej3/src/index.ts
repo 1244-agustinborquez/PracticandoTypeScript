407028c6b6bb6bfc2b1ea50b8f0c6bfbf3b7a769
Guillermo Agustín Borquez <agustinborquez1999@hotmail.com>
2021-10-04 03:29:21 -0300

    functions in TypeScript

---
+++ ej3/src/index.ts
@@ -0,0 +1,10 @@
+function saludar(nombre:string, edad:number):string {
+    return `Hola! Me llamo ${nombre} y tengo ${edad} años`;
+};
+
+
+const saludo = ():number => {
+    return 5
+};
+
+console.log(saludar('Agustin', 22));
